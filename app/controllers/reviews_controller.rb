83650e2a751832d462b86bf97807a6b24f97dda5
Jean Philippe Hicintuka <jeanphilh@outlook.com>
2019-11-20 20:26:59 +0800

    commiting the copy without design. will add design later

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,36 @@
+class ReviewsController < ApplicationController
+
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+    # @review.restaurant_id = @restaurant.id
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+end
